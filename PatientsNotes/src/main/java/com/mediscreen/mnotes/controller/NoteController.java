95dc7ee9defb367afd9fe69450ef20c18bbcd367
Laurence THIELLAUD <laurence.thiellaud@axa.fr>
2021-12-14 13:59:29 +0100

    Création des notes ajouté dans le microservice mnotes

--- PatientsNotes/src/main/java/com/mediscreen/mnotes/controller/NoteController.java
+++ PatientsNotes/src/main/java/com/mediscreen/mnotes/controller/NoteController.java
@@ -6,10 +6,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +21,8 @@ public class NoteController {
     
     @Autowired
     private NoteService noteService;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NoteController.class);
 
     @ApiOperation(value = "Liste des notes prises pour un patient")
     @ApiResponses(value = {
@@ -35,6 +38,17 @@ public class NoteController {
     @GetMapping(value = "/patHistory/all")
     public List<Note> getAllNotes() {
         return noteService.getAllNotes();
+    }
+
+    @ApiOperation(value = "Création d'une nouvelle note pour le patient {patId}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Created"),
+            @ApiResponse(code = 400, message = "Bad request")})
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value = "/patHistory/add")
+    public Note createNote(@RequestParam Integer patientId, @RequestParam String note) {
+        LOGGER.info("Note à ajouter pour le Patient id " + patientId);
+        return noteService.createNote(patientId, note);
     }
 
 }
