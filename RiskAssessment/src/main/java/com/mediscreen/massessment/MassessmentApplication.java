c138f9e931eac23fab9ceb0c0ce1c462cc6e4774
Laurence THIELLAUD <laurence.thiellaud@axa.fr>
2022-01-13 11:51:58 +0100

    Creation module RiskAssessment

---
+++ RiskAssessment/src/main/java/com/mediscreen/massessment/MassessmentApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.massessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassessmentApplication.class, args);
+	}
+
+}
